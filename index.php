f300d273fb44dc72904ef557d9b06ae4029fd368
Fakhri fauzan XIpplg2 <ffzanjan@gmail.com>
2026-09-21 14:15:16 +0700

    Add files via upload
    Fakhri fauzan Abdullah
    Tugas index.Php 21sept

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$sapa = "Darimu, 2000 tahun yang lalu";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" type="text/css" href="assets/style.css"></link>
+    <script src="JS_script/test.js"></script>
+    <h1>paget</h1>
+    <h2>Klik tombol dibawah ini</h2>
+    <button id="paget">Klik me</button> <br>
+
+    <main>
+        <p>Kepadamu, 2000 tahun mulai sekarang</p>
+        <?php echo $sapa ?>
+    </main>
+</body>
+</html>
